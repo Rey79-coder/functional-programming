c9c3fab10bba945912ba7e7042a900461a0e4802
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-08-03 20:29:29 -0700

    self-contained-btn

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-const buttonEl = document.getElementById('my-button');
+const containerEl = document.getElementById('container');
 
-const clickHandler = function() {
-  let count = 0;
-
-  return function() {
-    count++;
-    this.textContent = `Clicks: ${count}`;
-  };
+const clickHandler = function(event) {
+  if (event.target.matches('button')) {
+    event.target.textContent = 'Clicked!';
+  }
 };
 
-buttonEl.addEventListener('click', clickHandler());
+containerEl.addEventListener('click', clickHandler);
+
+
+
+
+// FUNCTION TO CREATE A COUNTER
+// const buttonEl = document.getElementById('my-button');
+
+// const clickHandler = function() {
+//   let count = 0;
+
+//   return function() {
+//     count++;
+//     this.textContent = `Clicks: ${count}`;
+//   };
+// };
+
+// buttonEl.addEventListener('click', clickHandler());
+
+
+
+
+
+
+
+
+// CLOSURE APPROACH IS THAT YOU ADD MORE BUTTONS, YOU'RE NOT POLLUTING THE GLOBAL SPACE.
+// const buttons = document.getElementsByTagName('button');
+
+// for (let i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener('click', clickHandler());
+// }
