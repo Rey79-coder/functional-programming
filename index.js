5ab53023699b26889d1b937b97fb075ee37129e9
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-08-05 18:36:22 -0700

    bubble/sorting

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 const Benchmark = require('benchmark');
-const { linearSearch, binarySearch } = require('./search');
+const { bubbleSort } = require('./sort');
 
 const numbers = [];
-for (let i = 1; i <= 4000000; i++) {
-  // populate the array with the numbers 1 to 1,000,000
-  numbers.push(i);
+for (let i = 0; i < 10000; i++) {
+  numbers.push(Math.floor(Math.random() * 10000) + 1);
 }
-
-// grab the last number in the array as the number we want to find
-const target = numbers[0];
 
 const suite = new Benchmark.Suite;
 
 suite
-  .add('linear search', function() {
-    linearSearch(numbers, target);
-  })
-  .add('binary search', function() {
-    binarySearch(numbers, target, 0, numbers.length-1)
+  .add('bubble sort', function() {
+    const testArray = [...numbers];
+    bubbleSort(testArray);
   })
   .on('complete', function() {
-    // loop over and print each result
     this.forEach(result => console.log(`${result.name} averaged ${result.stats.mean*1000} milliseconds.`));
   })
   .run();
+
+
+// const Benchmark = require('benchmark');
+// const { linearSearch, binarySearch } = require('./search');
+
+// const numbers = [];
+// for (let i = 1; i <= 4000000; i++) {
+//   // populate the array with the numbers 1 to 1,000,000
+//   numbers.push(i);
+// }
+
+// // grab the last number in the array as the number we want to find
+// const target = numbers[0];
+
+// const suite = new Benchmark.Suite;
+
+// suite
+//   .add('linear search', function() {
+//     linearSearch(numbers, target);
+//   })
+//   .add('binary search', function() {
+//     binarySearch(numbers, target, 0, numbers.length-1)
+//   })
+//   .on('complete', function() {
+//     // loop over and print each result
+//     this.forEach(result => console.log(`${result.name} averaged ${result.stats.mean*1000} milliseconds.`));
+//   })
+//   .run();
